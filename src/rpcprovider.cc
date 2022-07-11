9ba9ffc1cb9ce6dbdfc53ccea410c0cf0f867fc1
CloudzL <42520726@qq.com>
2022-07-11 23:28:59 +0800

    数据头类型及反序列化

--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -1,6 +1,6 @@
 #include "rpcprovider.h"
 #include "mprpcapplication.h"
-//#include"rpcheader.pb.h"
+#include "rpcheader.pb.h"
 //#include"zookeeperutil.h"
 
 /*
@@ -81,6 +81,51 @@ void RpcProvider::OnMessge(const muduo::net::TcpConnectionPtr &conn,
                            muduo::net::Buffer *buffer,
                            muduo::Timestamp)
 {
+    //获取到数据，即网络上接受的远程rpc调用请求的字符流， Login和args
+    std::string recv_buf = buffer->retrieveAllAsString();
+
+    // std::cout<<"已获取数据"<<std::endl;
+    //读取header_size，此时的整数若按照字符串格式发送，读取时会出现问题，所以需要直接按二进制发送
+    //从字符流中读取前四个字节的内容
+    uint32_t header_size = 0;
+    recv_buf.copy((char *)&header_size, 4, 0);
+    // std::cout<<"已读取头信息"<<std::endl;
+    //根据header_size读取数据头的原始字符流，反序列化数据，得到rpc的详细请求数据
+    std::string rpc_header_str = recv_buf.substr(4, header_size); // substr从4开始读读取header_size个字节的数据
+    mprpc::RpcHeader rpcHeader;
+    std::string service_name; //用于存储反序列化成功的服务名字
+    std::string method_name;  //用于存储反序列化成功的服务方法
+    uint32_t args_size;       //用于存储反序列化成功的参数个数
+    // std::cout<<"已读取头信息"<<std::endl;
+    std::cout << "======================================" << std::endl;
+    std::cout << "header_size: " << header_size << std::endl;
+    std::cout << "rpc_header_str" << rpc_header_str << std::endl;
+    std::cout << "======================================" << std::endl;
+    if (rpcHeader.ParseFromString(rpc_header_str)) //开始反序列化，参数接受类型为引用,返回值为bool型
+    {
+        //数据头反序列化成功
+        service_name = rpcHeader.service_name();
+        method_name = rpcHeader.method_name();
+        args_size = rpcHeader.args_size();
+    }
+    else
+    {
+        //数据头反序列化失败
+        std::cout << "rpc_header_str: " << rpc_header_str << "parse error!" << std::endl;
+        return;
+    }
+
+    //获取rpc参数方法的字符流数据
+    std::string args_str = recv_buf.substr(4 + header_size, args_size);
+
+    //打印调试信息
+    std::cout << "======================================" << std::endl;
+    std::cout << "header_size: " << header_size << std::endl;
+    std::cout << "rpc_header_str" << rpc_header_str << std::endl;
+    std::cout << "service_name: " << service_name << std::endl;
+    std::cout << "method_name: " << method_name << std::endl;
+    std::cout << "args_str: " << args_str << std::endl;
+    std::cout << "======================================" << std::endl;
 }
 
 // Closure的回调操作，用于序列化rpc的相应和网络发送
